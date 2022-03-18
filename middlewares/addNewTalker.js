3937b3090f994eacd322ccbc3f547b0f47e46cbe
vitor <vitorsbsilva@gmail.com>
2022-03-18 11:51:59 -0300

    Create validateName.js function to check if the name received in the body requisition match all the parameters

--- middlewares/addNewTalker.js
+++ middlewares/addNewTalker.js
@@ -14,12 +14,32 @@ const validateToken = (token) => {
   return 'ok';
 };
 
+const validateName = (name) => {
+  if (!name) {
+    const message = { message: 'O campo "name" é obrigatório' };
+    return message;
+  }
+
+  if (name.length < 3) {
+    const message = { message: 'O "name" deve ter pelo menos 3 caracteres' };
+    return message;
+  }
+
+  return 'ok';
+};
+
 const addNewTalker = (req, res) => {
   const { token } = req.headers;
+  const { name } = req.body;
 
   const tokenValidation = validateToken(token);
   if (tokenValidation !== 'ok') {
     return res.status(401).send(tokenValidation);
+  }
+
+  const nameValidation = validateName(name);
+  if (nameValidation !== 'ok') {
+    return res.status(400).send(nameValidation);
   }
 };
 
